3a05b5204a68a1e584b52ca83447f5be03f86fbf
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-07-31 15:44:34 -0300

    CRiados dados mockados

---
+++ spa-noticias/src/Datas.js
@@ -0,0 +1,37 @@
+export const news = [
+    {
+        title: "noticia 1",
+        text: "Lorem ipsum dolor sit amet. Qui inventore nesciunt ab quod suscipit ut recusandae eius quo neque doloribus. Sed laudantium aperiam qui doloribus vero eum fugiat officiis. Nam voluptates rerum rem aspernatur expedita et repellat ipsa.",
+        image: "https://gluby.com.br/storage/uploads/blog/posts/20230126_07295643357.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "noticia 2",
+        text: "Lorem ipsum dolor sit amet. Qui inventore nesciunt ab quod suscipit ut recusandae eius quo neque doloribus. Sed laudantium aperiam qui doloribus vero eum fugiat officiis. Nam voluptates rerum rem aspernatur expedita et repellat ipsa.",
+        image: "https://gluby.com.br/storage/uploads/blog/posts/20230126_07295643357.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "noticia 3",
+        text: "Lorem ipsum dolor sit amet. Qui inventore nesciunt ab quod suscipit ut recusandae eius quo neque doloribus. Sed laudantium aperiam qui doloribus vero eum fugiat officiis. Nam voluptates rerum rem aspernatur expedita et repellat ipsa.",
+        image: "https://gluby.com.br/storage/uploads/blog/posts/20230126_07295643357.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "noticia 4",
+        text: "Lorem ipsum dolor sit amet. Qui inventore nesciunt ab quod suscipit ut recusandae eius quo neque doloribus. Sed laudantium aperiam qui doloribus vero eum fugiat officiis. Nam voluptates rerum rem aspernatur expedita et repellat ipsa.",
+        image: "https://gluby.com.br/storage/uploads/blog/posts/20230126_07295643357.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "noticia 5",
+        text: "Lorem ipsum dolor sit amet. Qui inventore nesciunt ab quod suscipit ut recusandae eius quo neque doloribus. Sed laudantium aperiam qui doloribus vero eum fugiat officiis. Nam voluptates rerum rem aspernatur expedita et repellat ipsa.",
+        image: "https://gluby.com.br/storage/uploads/blog/posts/20230126_07295643357.png",
+        likes: 2,
+        comments: 4
+    }
+]
